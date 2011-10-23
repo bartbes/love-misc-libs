3adfb5d0dd2f65fffb47bc3ea35251d631184e4b
Bart van Strien <bart@bartbes.ath.cx>
2011-10-23 18:00:47 +0200

    More tests

--- LUBE/tests/tests.lua
+++ LUBE/tests/tests.lua
@@ -1,25 +1,147 @@
-UnitTest("Generic Client instantiation failure", function()
+class "lube.testClient" ("lube.Client") {
+	_implemented = true,
+
+	createSocket = function(self)
+		self.socket = true
+	end,
+
+	_connect = function(self)
+		self.connected = {host = self.host, port = self.port}
+		return true
+	end,
+
+	_disconnect = function(self)
+		self.connected = false
+	end,
+
+	_send = function(self, data)
+		self.sent = data
+		return true
+	end,
+
+	_receive = function(self)
+		local msg = self.received
+		self.received = nil
+		return msg
+	end,
+
+	setOption = function(self, option, value)
+		local allowed = {
+			broadcast = true
+		}
+		assert(allowed[option])
+	end
+}
+
+class "lube.testServer" ("lube.Server") {
+	_implemented = true,
+
+	createSocket = function(self)
+		self.socket = true
+	end,
+
+	_listen = function(self)
+		self.listening = self.port
+	end,
+
+	send = function(self, data, clientid)
+		self.sent = {data = data, clientid = clientid}
+	end,
+
+	receive = function(self)
+		local msg = self.received
+		self.received = nil
+		return msg.data, msg.clientid
+	end,
+
+	accept = function(self)
+		self.accepted = true
+	end,
+}
+
+UnitTest("Generic Client instantiation", function()
 	assert(not pcall(lube.Client))
 end)
 
-UnitTest("Generic Server instantiation failure", function()
+UnitTest("Generic Server instantiation", function()
 	assert(not pcall(lube.Server))
 end)
 
-UnitTest("Successful UdpClient instantiation", function()
+UnitTest("UdpClient instantiation", function()
 	assert(lube.udpClient())
 end)
 
-UnitTest("Successful TcpClient instantiation", function()
+UnitTest("TcpClient instantiation", function()
 	assert(lube.tcpClient())
 end)
 
-UnitTest("Successful UdpServer instantiation", function()
+UnitTest("UdpServer instantiation", function()
 	assert(lube.udpServer())
 end)
 
-UnitTest("Successful TcpServer instantiation", function()
+UnitTest("TcpServer instantiation", function()
 	assert(lube.tcpServer())
+end)
+
+UnitTest("TestClient instantiation", function()
+	assert(lube.testClient())
+end)
+
+UnitTest("TestServer instantiation", function()
+	assert(lube.testServer())
+end)
+
+UnitTest("Client connect handshake", function()
+	local client = lube.testClient()
+	client.handshake = "handshake"
+
+	client:connect("127.0.0.1", 9797, false)
+
+	assert(client.sent == "handshake+\n")
+end)
+
+UnitTest("Client disconnect handshake", function()
+	local client = lube.testClient()
+	client.handshake = "handshake"
+
+	client:connect("127.0.0.1", 9797, false)
+	client:disconnect()
+
+	assert(client.sent == "handshake-\n")
+end)
+
+UnitTest("Client send", function()
+	local client = lube.testClient()
+	client:connect("127.0.0.1", 9797, false)
+
+	client:send("hellothere")
+
+	assert(client.sent == "hellothere")
+end)
+
+UnitTest("Client receive", function()
+	local client = lube.testClient()
+	client:connect("127.0.0.1", 9797, false)
+
+	client.received = "hellothere"
+
+	assert(client:receive() == "hellothere")
+end)
+
+UnitTest("Client recv callback", function()
+	local client = lube.testClient()
+	client:connect("127.0.0.1", 9797, false)
+
+	client.received = "hellothere"
+	local called = false
+	client.callbacks.recv = function(data)
+		called = true
+		assert(data == "hellothere")
+	end
+
+	client:update(5)
+
+	assert(called)
 end)
 
 ImplTest("Udp client connect handshake", function()
@@ -83,7 +205,6 @@ ImplTest("Udp client disconnect handshake", function()
 	log("Disconnect handshake: " .. msg:gsub("\n", "\\n"))
 
 	assert(msg:match("handshake%-\n?"))
-	assert(msg == "handshake-\n")
 end)
 
 ImplTest("Tcp client disconnect handshake", function()
@@ -106,4 +227,132 @@ ImplTest("Tcp client disconnect handshake", function()
 	log("Disconnect handshake: " .. msg:gsub("\n", "\\n"))
 
 	assert(msg:match("handshake%-\n?"))
+end)
+
+ImplTest("Udp client recv callback", function()
+	local serv = socket.udp()
+	serv:setsockname("*", 9797)
+	serv:settimeout(5)
+
+	local client = lube.udpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	local _, ip, port = serv:receivefrom()
+	log(("Message from %s:%d"):format(ip, port))
+	serv:sendto("hellothere", ip, port)
+
+	local called = false
+	client.callbacks.recv = function(data)
+		log("Message: " .. data)
+		called = true
+		assert(data == "hellothere")
+	end
+	client:update(5)
+
+	assert(called)
+end)
+
+ImplTest("Udp client receive", function()
+	local serv = socket.udp()
+	serv:setsockname("*", 9797)
+	serv:settimeout(5)
+
+	local client = lube.udpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	local _, ip, port = serv:receivefrom()
+	log(("Message from %s:%d"):format(ip, port))
+	serv:sendto("hellothere", ip, port)
+
+	local msg = client:receive()
+	log("Message: " .. msg)
+	assert(msg == "hellothere")
+end)
+
+ImplTest("Tcp client recv callback", function()
+	local serv = socket.tcp()
+	serv:setoption("reuseaddr", true)
+	serv:settimeout(5)
+	serv:bind("*", 9797)
+	serv:listen(1)
+
+	local client = lube.tcpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	local c = serv:accept()
+
+	c:send("hellothere")
+
+	local called = false
+	client.callbacks.recv = function(data)
+		log("Message: " .. data)
+		called = true
+		assert(data == "hellothere")
+	end
+	client:update(5)
+
+	assert(called)
+end)
+
+ImplTest("Tcp client receive", function()
+	local serv = socket.tcp()
+	serv:setoption("reuseaddr", true)
+	serv:settimeout(5)
+	serv:bind("*", 9797)
+	serv:listen(1)
+
+	local client = lube.tcpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	local c = serv:accept()
+
+	c:send("hellothere")
+
+	local msg = client:receive()
+	log("Message: " .. msg)
+	assert(msg == "hellothere")
+end)
+
+ImplTest("Tcp client send", function()
+	local serv = socket.tcp()
+	serv:setoption("reuseaddr", true)
+	serv:settimeout(5)
+	serv:bind("*", 9797)
+	serv:listen(1)
+
+	local client = lube.tcpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	local c = serv:accept()
+	c:receive()
+
+	client:send("hellothere\n")
+
+	local msg = c:receive()
+	log("Message: " .. msg)
+	assert(msg == "hellothere\n", "Weird luasocket thing, unsolved bug") --so.. what goes wrong here?
+	--it's something odd with luasocket
+end)
+
+ImplTest("Udp client send", function()
+	local serv = socket.udp()
+	serv:setsockname("*", 9797)
+	serv:settimeout(5)
+
+	local client = lube.udpClient()
+	client.handshake = "handshake"
+	client:connect("127.0.0.1", 9797, false)
+
+	serv:receive()
+
+	client:send("hellothere")
+
+	local msg = serv:receive()
+	log("Message: " .. msg)
+	assert(msg == "hellothere")
 end)
