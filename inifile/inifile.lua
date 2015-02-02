dc5eba18c6ecd844f4c3e50c1f6d102c8138eec3
Bart van Strien <bart.bes@gmail.com>
2015-02-02 19:45:43 +0100

    Update inifile to return a local table with meta-information, dub it version 1.0

--- inifile/inifile.lua
+++ inifile/inifile.lua
@@ -1,32 +1,35 @@
--- Copyright 2011-2013 Bart van Strien. All rights reserved.
--- 
--- Redistribution and use in source and binary forms, with or without modification, are
--- permitted provided that the following conditions are met:
--- 
---    1. Redistributions of source code must retain the above copyright notice, this list of
---       conditions and the following disclaimer.
--- 
---    2. Redistributions in binary form must reproduce the above copyright notice, this list
---       of conditions and the following disclaimer in the documentation and/or other materials
---       provided with the distribution.
--- 
--- THIS SOFTWARE IS PROVIDED BY BART VAN STRIEN ''AS IS'' AND ANY EXPRESS OR IMPLIED
--- WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
--- FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BART VAN STRIEN OR
--- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
--- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
--- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
--- ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
--- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
--- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--- 
--- The views and conclusions contained in the software and documentation are those of the
--- authors and should not be interpreted as representing official policies, either expressed
--- or implied, of Bart van Strien.
---
--- The above license is known as the Simplified BSD license.
+local inifile = {
+	_VERSION = "inifile 1.0",
+	_DESCRIPTION = "Inifile is a simple, complete ini parser for lua",
+	_URL = "http://docs.bartbes.com/inifile",
+	_LICENSE = [[
+		Copyright 2011-2015 Bart van Strien. All rights reserved.
 
-inifile = {}
+		Redistribution and use in source and binary forms, with or without modification, are
+		permitted provided that the following conditions are met:
+
+		   1. Redistributions of source code must retain the above copyright notice, this list of
+			  conditions and the following disclaimer.
+
+		   2. Redistributions in binary form must reproduce the above copyright notice, this list
+			  of conditions and the following disclaimer in the documentation and/or other materials
+			  provided with the distribution.
+
+		THIS SOFTWARE IS PROVIDED BY BART VAN STRIEN ''AS IS'' AND ANY EXPRESS OR IMPLIED
+		WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+		FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BART VAN STRIEN OR
+		CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+		CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+		SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+		ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+		NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+		ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+		The views and conclusions contained in the software and documentation are those of the
+		authors and should not be interpreted as representing official policies, either expressed
+		or implied, of Bart van Strien.
+	]] -- The above license is known as the Simplified BSD license.
+}
 
 local defaultBackend = "io"
 
